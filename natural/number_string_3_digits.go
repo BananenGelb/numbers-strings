5fafc8ce0bb77fa86d973f93a5b290a3d40dd37f
BananenGelb <Benedict.lotter@gmx.de>
2024-12-23 11:41:15 +0100

    23-12-2024

--- natural/number_string_3_digits.go
+++ natural/number_string_3_digits.go
@@ -3,5 +3,24 @@ package natural
 // NumberString3Digits erwartet eine Zahl 0 <= n <= 999 und liefert den zugehörigen String.
 func NumberString3Digits(n int) string {
 	// TODO
-	return ""
+
+	zahleneinsbisneunzehn := []string{"eins", "zwei", "drei", "vier", "fünf", "sechs", "sieben", "acht", "neun", "zehn", "elf", "zwölf", "dreizehn", "vierzehn", "fünfzehn", "sechzehn", "siebzehn", "achtzehn", "neunzehn"}
+	zahlnull := "null"
+
+	if n == 0 {
+		return zahlnull
+	}
+	if n > 0 && n < 20 {
+		return zahleneinsbisneunzehn[n]
+	}
+
+	result := []int{}
+	for n != 0 {
+		r := n % 10
+		n = n / 10
+
+		result = append(result, r)
+	}
+
+	//return zahlen[n]
 }
